76bbf4ea948a0bee5995a7ed00b87e11f9b052d9
Vecoza <146360710+Vecoza@users.noreply.github.com>
2024-11-19 16:56:58 +0100

    Popravljen slider

--- src/app/modules/public/view/home/home.component.ts
+++ src/app/modules/public/view/home/home.component.ts
@@ -1,34 +1,24 @@
-import {AfterViewInit, Component} from '@angular/core';
-import {NgForOf} from '@angular/common';
-import {FeaturesPieceComponent} from '../../component/features-piece/features-piece.component';
-import {AboutPieceComponent} from '../../component/about-piece/about-piece.component';
-import {FactCounterComponent} from '../../component/fact-counter/fact-counter.component';
-import {ServicePieceComponent} from '../../component/service-piece/service-piece.component';
-import {CategoriesPieceComponent} from '../../component/categories-piece/categories-piece.component';
-import {CentralProcessComponent} from '../../component/central-process/central-process.component';
-import {BlogPiecesComponent} from '../../component/blog-pieces/blog-pieces.component';
-import {BannerComponent} from '../../component/banner/banner.component';
+import {Component, AfterViewInit, OnInit, OnDestroy} from '@angular/core';
+import { NgForOf } from '@angular/common';
 import {TeamsComponent} from '../../component/teams/teams.component';
+import {BannerComponent} from '../../component/banner/banner.component';
+import {BlogPiecesComponent} from '../../component/blog-pieces/blog-pieces.component';
+import {CentralProcessComponent} from '../../component/central-process/central-process.component';
+import {CategoriesPieceComponent} from '../../component/categories-piece/categories-piece.component';
+import {ServicePieceComponent} from '../../component/service-piece/service-piece.component';
+import {FactCounterComponent} from '../../component/fact-counter/fact-counter.component';
+import {AboutPieceComponent} from '../../component/about-piece/about-piece.component';
+import {FeaturesPieceComponent} from '../../component/features-piece/features-piece.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  standalone:true,
-  imports: [
-    NgForOf,
-    FeaturesPieceComponent,
-    AboutPieceComponent,
-    FactCounterComponent,
-    ServicePieceComponent,
-    CategoriesPieceComponent,
-    CentralProcessComponent,
-    BlogPiecesComponent,
-    BannerComponent,
-    TeamsComponent
-  ]
+  standalone: true,
+  imports: [NgForOf, TeamsComponent, BannerComponent, BlogPiecesComponent, CentralProcessComponent, CategoriesPieceComponent, ServicePieceComponent, FactCounterComponent, AboutPieceComponent, FeaturesPieceComponent]
 })
-export class HomeComponent{
+//
+export class HomeComponent implements OnInit, OnDestroy{
   carouselItems = [
     {
       image: '/assets/img/carousel-2.jpg',
@@ -50,14 +40,32 @@ export class HomeComponent{
     }
   ];
 
-  // ngAfterViewInit(): void {
-  //   const carouselElement = document.getElementById('carouselId');
-  //   if (carouselElement) {
-  //     const carousel = new bootstrap.Carousel(carouselElement, {
-  //       interval: 30000, // 30 seconds
-  //       ride: 'carousel'
-  //     });
-  //   }
-  // }
+  activeIndex = 0;  // To track the active slide
+  intervalId: any;
 
+  ngOnInit(): void {
+    this.startCarousel();
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  startCarousel(): void {
+    this.intervalId = setInterval(() => {
+      this.activeIndex = (this.activeIndex + 1) % this.carouselItems.length;
+    }, 30000); // Slide changes every 30 seconds
+  }
+
+  goToSlide(index: number): void {
+    if (index < 0) {
+      this.activeIndex = this.carouselItems.length - 1;
+    } else if (index >= this.carouselItems.length) {
+      this.activeIndex = 0;
+    } else {
+      this.activeIndex = index;
+    }
+  }
 }
